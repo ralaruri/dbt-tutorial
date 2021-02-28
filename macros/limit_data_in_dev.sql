22d3ed30b9a693be98e3a3c31d2176f6c489e2df
Ramzi Al-Aruri <ramzi@Ramzis-MBP.fios-router.home>
2021-02-28 01:09:01 -0500

    walking through some jinja examples

---
+++ macros/limit_data_in_dev.sql
@@ -0,0 +1,7 @@
+{% macro limit_data_in_dev(column_name, dev_days_of_data = 3) %}
+
+{% if target.name == 'dev' %}
+where {{ column_name }} >= date_add(current_date(), interval -{{ dev_days_of_data }} DAY)
+{% endif %}
+
+{% endmacro %}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -6,7 +6,7 @@ with payment as (
       paymentmethod as payment_method,
       status,
       --amount is stored in cents, convert to dollars
-      amount / 100 as amount,
+      {{ cents_to_dollars('amount', 2) }} as amount,
       created as created_at
 
   from {{ source('stripe', 'payment') }}
